91cd83cc3fdb3978eb08c20330259730d574148a
ahmadbadawikr <ahmadbadawikr@outlook.com>
2020-08-04 22:37:21 +0700

    fixing dashboard

--- class-notes/app/admin/dashboard.rb
+++ class-notes/app/admin/dashboard.rb
@@ -9,24 +9,32 @@ ActiveAdmin.register_page "Dashboard" do
       end
     end
 
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
+    columns do
+      column do
+        panel 'Recent Notes' do
+          ul do
+            Note.order(created_at: :desc).first(5).map do |note|
+              li link_to(note.title, admin_note_path(note))
+            end
+          end
+        end
+      end
+      column do
+        panel 'Info' do
+          para 'Welcome to your notes app'
+        end
+      end
+    end
+    columns do
+      column do
+        panel 'Your loyal subjects' do
+          ul do
+            Subject.order(name: :asc).map do |subject|
+              li link_to(subject.name, admin_subject_path(subject))
+            end
+          end
+        end
+      end
+    end
   end # content
 end
--- class-notes/app/assets/javascripts/active_admin.js
+++ class-notes/app/assets/javascripts/active_admin.js
@@ -1,3 +1,5 @@
 //= require active_admin/base
 //= require activeadmin_addons/all
 //= require active_material
+//= require activeadmin/quill_editor/quill
+//= require activeadmin/quill_editor_input
